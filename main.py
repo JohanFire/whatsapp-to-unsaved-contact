2aea4dcbefcb593d45c9bf4a7181e2b29cd34654
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-29 15:09:10 -0600

    first Tkinter app

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+# from tkinter import messagebox
+
+
+
+window = tkinter.Tk()
+window.title("WhatsApp to Unsaved Contact")
+window.geometry('340x440')
+window.configure(bg='#333333')
+
+window.mainloop()
