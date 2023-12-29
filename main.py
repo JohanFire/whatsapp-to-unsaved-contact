499626c3e938a599665f5848af2f6ecef0251e14
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-29 15:10:21 -0600

    adding main method

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter
 # from tkinter import messagebox
 
+def main():
+    app = tkinter.Tk()
+    app.title("WhatsApp to Unsaved Contact")
+    app.geometry('340x440')
+    app.configure(bg='#333333')
 
+    app.mainloop()
 
-window = tkinter.Tk()
-window.title("WhatsApp to Unsaved Contact")
-window.geometry('340x440')
-window.configure(bg='#333333')
-
-window.mainloop()
+if __name__ == '__main__':
+    main()
