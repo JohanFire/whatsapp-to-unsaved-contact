f7eb59f49855d2271335694a7925e012108f011a
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-29 18:57:02 -0600

    trying Combobox or OptionMenu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 # from tkinter import messagebox
 # from typing import Tuple
 from icecream import ic
@@ -17,24 +18,44 @@ def main():
     frame = tkinter.Frame(bg='#333333')
 
     """ Widgets """
-    login_label  = tkinter.Label(
+    title_label  = tkinter.Label(
         frame, text='WhatsApp to Unsaved Contact', bg='#333333', fg='#FF3399', font=("Arial", 30)
-        ).grid(row=0, column=0, columnspan=2, sticky='news', pady=40)
+        ).grid(row=0, column=1, sticky='news', pady=40)
+
+    """ 
     # username_label = tkinter.Label(
     #     frame, text='Phone Number:', bg='#333333', fg='#FFFFFF', font=("Arial", 20)
     #     ).grid(row=1, column=0, padx=20)
-
+    
     opciones = [52, '01', 48]
     options = tkinter.StringVar(frame)
     options.set(opciones[0])
-    lada_cmbbox = tkinter.OptionMenu(frame, options, *opciones
-        ).grid(row=1, column=0, padx=0)
+    # lada_optionMenu = tkinter.OptionMenu(frame, options, *opciones, 
+    #     ).grid(row=1, column=0, padx=0)
+
+    # Crear un estilo para personalizar el menú desplegable
+    estilo = ttk.Style()
+    estilo.theme_create("mi_tema", parent="alt", settings={
+        "TCombobox": {
+            "configure": {"fieldbackground": '#333333', "background": '#C0C0C0', "foreground": "#FFFFFF",
+                "font": ('Helvetica', 22)
+            },
+        }
+    })
+    # estilo.configure('TCombobox', font=('Helvetica', 22))
+    estilo.theme_use("mi_tema")
+
+
+    lada_cmbbox = ttk.Combobox(frame, values=opciones, textvariable=options, style='TCombobox')
+    lada_cmbbox.grid(row=1, column=0, padx=0)
+    """
+
 
     username_entry = tkinter.Entry(frame, font=("Arial", 20)
         ).grid(row=1, column=1, pady=20)
     login_button = tkinter.Button(
         frame, text='Send message', bg='#FF3399', fg='#FFFFFF', font=("Arial", 18)
-        ).grid(row=3, column=0, columnspan=2, pady=30)
+        ).grid(row=3, column=1, columnspan=2, pady=30)
 
     frame.pack()
 
