6e717d44d8fe07f06134f37b3f329df8824baad1
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-30 01:08:28 -0600

    improving logic to automatically format when 10 digits on entry

--- utils/App.py
+++ utils/App.py
@@ -14,13 +14,8 @@ class App:
             self.frame, text='WhatsApp to Unsaved Contact', bg='#333333', fg='#FF3399', font=("Arial", 30)
             ).grid(row=0, column=1, sticky='news', pady=40)
         
-        self.phone_number_entry = tkinter.Entry(self.frame, font=("Arial", 20), validate='key', validatecommand=(self.validate_phone_entry, '%P'))
+        self.phone_number_entry = tkinter.Entry(self.frame, font=("Arial", 20), validate='key')
         self.phone_number_entry['validatecommand'] = (self.phone_number_entry.register(self.validate_phone_digit), '%P')        
-        
-        # Formatear automáticamente el número al estilo "XX XXXX XXXX"
-        self.phone_number_entry.bind('<KeyRelease>', self.format_phone_number)
-        # self.phone_number_entry.bind('<KeyRelease>', self.validate_phone_format)
-        
         self.phone_number_entry.grid(row=1, column=1, pady=20)
 
         self.send_message_button = tkinter.Button(
@@ -52,8 +47,18 @@ class App:
             if len(digit) >= 13:
                 # self.phone_number_entry.configure(state='disabled')
                 return False
+            
         else:
-            if len(digit) >= 11:
+            if len(digit) == 10:
+                digit_spaced = digit[:2] + ' ' + digit[2: ]
+                digit_spaced = digit_spaced[:7] + ' ' + digit_spaced[7: ]
+                print(digit_spaced)
+
+                self.phone_number_entry.delete(0, 'end')
+                self.phone_number_entry.insert(0, digit_spaced)
+
+                # return False
+            elif len(digit) > 10:
                 return False
         
         validated_digit = digit
@@ -62,50 +67,7 @@ class App:
         # Verify if phone number matchs with the pattern
         if pattern.match(validated_digit):
             return True
+        elif validated_digit == "":
+            return True
         else:
             return False
-    
-    def validate_phone_entry(self, new_text):
-        """ Validate the length of the phone number entry """
-        return len(new_text) <= 12
-
-    def format_phone_number(self, event):
-        # Formatear automáticamente el número al estilo "XX XXXX XXXX"
-        current_text = self.phone_number_entry.get()
-        formatted_text = self.format_as_phone_number(current_text)
-        # self.phone_number_entry.delete(0, 'end')
-        self.phone_number_entry.insert(0, formatted_text)
-
-    def format_as_phone_number(self, raw_text):
-        # Formatear el número al estilo "XX XXXX XXXX"
-        digits = [char for char in raw_text if char.isdigit()]
-        formatted_text = ''
-
-        if digits[-1] and digits[-2] == " ":
-            digits_without_double_space = digits[-1]
-            digits = digits_without_double_space
-
-        if len(digits) == 2:
-            if digits[-1] == ' ':
-                print(digits[-1])
-                return
-
-            print('len(digits) == 2')
-            # formatted_text += ''.join(digits[0:2]) + ' '
-            
-            new_text = raw_text + ' '
-            self.phone_number_entry.delete(0, 'end')
-            self.phone_number_entry.insert(2, ' ')
-
-        elif len(digits) == 6:
-            if digits[-1] == ' ':
-                print(digits[-1])
-                return
-
-            print('len(digits) == 6')
-            # formatted_text += ''.join(digits[2:6]) + ' '
-
-            self.phone_number_entry.insert(7, ' ')
-
-
-        return formatted_text
