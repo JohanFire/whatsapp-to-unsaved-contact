463d72408431734d58b6916ec6f2f9656eadb65b
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-29 19:09:01 -0600

    moving everying to an App Class

--- main.py
+++ main.py
@@ -4,32 +4,21 @@ from tkinter import ttk
 # from typing import Tuple
 from icecream import ic
 
+from utils.App import App
 from utils.screen import render_center_of_screen
 
 def main():
-    app = tkinter.Tk()
+    root = tkinter.Tk()
 
-    app.title("WhatsApp to Unsaved Contact")
-    width, height, x, y = render_center_of_screen(app, width=800, height=340)
-    app.geometry(f"{width}x{height}+{x}+{y}")
-    app.resizable(width=False, height=False)
-    app.configure(bg='#333333')
+    root.title("WhatsApp to Unsaved Contact")
+    width, height, x, y = render_center_of_screen(root, width=800, height=340)
+    root.geometry(f"{width}x{height}+{x}+{y}")
+    root.resizable(width=False, height=False)
+    root.configure(bg='#333333')
 
-    frame = tkinter.Frame(bg='#333333')
+    app = App(root)
 
-    """ Frame Widgets """
-    title_label  = tkinter.Label(
-        frame, text='WhatsApp to Unsaved Contact', bg='#333333', fg='#FF3399', font=("Arial", 30)
-        ).grid(row=0, column=1, sticky='news', pady=40)
-    phone_number_entry = tkinter.Entry(frame, font=("Arial", 20)
-        ).grid(row=1, column=1, pady=20)
-    send_message_button = tkinter.Button(
-        frame, text='Send message', bg='#FF3399', fg='#FFFFFF', font=("Arial", 18)
-        ).grid(row=3, column=1, columnspan=2, pady=30)
-
-    frame.pack()
-
-    app.mainloop()
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
---
+++ utils/App.py
@@ -0,0 +1,29 @@
+import tkinter
+import webbrowser
+
+class App:
+    def __init__(self, master):
+        self.master = master
+        master.title("WhatsApp to Unsaved Contact")
+
+        self.frame = tkinter.Frame(bg='#333333')
+
+        """ Frame Widgets """
+        self.title_label  = tkinter.Label(
+            self.frame, text='WhatsApp to Unsaved Contact', bg='#333333', fg='#FF3399', font=("Arial", 30)
+            ).grid(row=0, column=1, sticky='news', pady=40)
+        self.phone_number_entry = tkinter.Entry(self.frame, font=("Arial", 20)
+            ).grid(row=1, column=1, pady=20)
+        self.send_message_button = tkinter.Button(
+            self.frame, text='Send message', bg='#FF3399', fg='#FFFFFF', font=("Arial", 18),
+            command=self.send_message_action
+            ).grid(row=3, column=1, columnspan=2, pady=30)
+
+        self.frame.pack()
+
+    def send_message_action(self):
+        # Especifica el enlace que deseas abrir
+        enlace = "https://www.google.com"
+
+        # Abre el enlace en el navegador web predeterminado
+        webbrowser.open(enlace)
