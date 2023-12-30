8bfb89771e0afadf8caecd2f97364ece648931af
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-29 18:15:56 -0600

    simplifying geometry instances

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from utils.screen import render_center_of_screen
 
 def main():
     app = tkinter.Tk()
+
     app.title("WhatsApp to Unsaved Contact")
     width, height, x, y = render_center_of_screen(app, width=800, height=640)
     app.geometry(f"{width}x{height}+{x}+{y}")
@@ -17,23 +18,21 @@ def main():
 
     """ Widgets """
     login_label  = tkinter.Label(
-        frame, text='Login', bg='#333333', fg='#FF3399', font=("Arial", 30) )
+        frame, text='Login', bg='#333333', fg='#FF3399', font=("Arial", 30)
+        ).grid(row=0, column=0, columnspan=2, sticky='news', pady=40)
     username_label = tkinter.Label(
-        frame, text='Username', bg='#333333', fg='#FFFFFF', font=("Arial", 16) )
-    username_entry = tkinter.Entry(frame, font=("Arial", 16) )
+        frame, text='Username', bg='#333333', fg='#FFFFFF', font=("Arial", 16)
+        ).grid(row=1, column=0)
+    username_entry = tkinter.Entry(frame, font=("Arial", 16)
+        ).grid(row=1, column=1, pady=20)
     password_label = tkinter.Label(
-        frame, text='Password', bg='#333333', fg='#FFFFFF', font=("Arial", 16) )
-    password_entry = tkinter.Entry(frame, show='*', font=("Arial", 16) )
+        frame, text='Password', bg='#333333', fg='#FFFFFF', font=("Arial", 16)
+        ).grid(row=2, column=0)
+    password_entry = tkinter.Entry(frame, show='*', font=("Arial", 16)
+        ).grid(row=2, column=1, pady=20)
     login_button = tkinter.Button(
-        frame, text='Login', bg='#FF3399', fg='#FFFFFF', font=("Arial", 16) )
-
-    """ Placing widgets on screen """
-    login_label.grid(row=0, column=0, columnspan=2, sticky='news', pady=40)
-    username_label.grid(row=1, column=0)
-    username_entry.grid(row=1, column=1, pady=20)
-    password_label.grid(row=2, column=0)
-    password_entry.grid(row=2, column=1, pady=20)
-    login_button.grid(row=3, column=0, columnspan=2, pady=30)
+        frame, text='Login', bg='#FF3399', fg='#FFFFFF', font=("Arial", 16)
+        ).grid(row=3, column=0, columnspan=2, pady=30)
 
     frame.pack()
 
