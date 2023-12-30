ffe3569f503716b485078d84b1f2e59a3a94ebf3
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-29 18:08:43 -0600

    utils directory and files

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 import tkinter
 # from tkinter import messagebox
 # from typing import Tuple
-
 from icecream import ic
 
-def render_center_of_screen(app, width: int, height: int) -> tuple[int, int, int, int]:
-    """ Calculate X and Y coordinates to center window 
-    ---
-    ``x = (screen_width - width) // 2`` \n
-    ``y = (screen_height - height) // 2`` \n
-    """
-    screen_width = app.winfo_screenwidth()
-    screen_height = app.winfo_screenheight()
-
-    # Calculate X and Y coordinates to center window
-    x = (screen_width - width) // 2
-    y = (screen_height - height) // 2
-
-    return width, height, x, y
-
+from utils.screen import render_center_of_screen
 
 def main():
     app = tkinter.Tk()
